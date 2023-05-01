6f7e408afd902aca6705b41c87168326917cf4fd
kumarniket <niketkr18@gmail.com>
2023-05-01 21:08:17 +0530

    first commit

--- JDBCJavaCRUD/src/com/JDBC/Connectivity/Main.java
+++ JDBCJavaCRUD/src/com/JDBC/Connectivity/Main.java
@@ -4,7 +4,8 @@ public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		//this is
+		//this is testing
+		
 		
 		//new DBManager().selectRecord();
 		CRUDOperationJava bss = new CRUDOperationJava();
